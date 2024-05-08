9c20623cadb720cc809e801cc30d9fece98f9cd8
Ajay09shinde <158314508+Ajay09shinde@users.noreply.github.com>
2024-05-09 00:50:39 +0530

    Update index.js

--- 06_node/index.js
+++ 06_node/index.js
@@ -1,8 +1,19 @@
 const express = require('express');
+const path = require('path'); 
+
 const app = express();
-app.use(express.static('public'));
+const PORT = 5500;
 
 
-app.listen(4000,()=>{
-    console.log("server is loaded");
-})
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    
+    res.sendFile(path.join(__dirname, 'public', 'portfolio.html'));
+});
+
+
+
+app.listen(PORT, () => {
+    console.log('Server is started');
+});
